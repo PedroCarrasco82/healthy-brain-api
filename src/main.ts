89469e46ff8fdb17668a8e6cbc304376c7a6bc02
PedroCarrasco82 <contatopedrocarrasco@gmail.com>
2021-04-23 20:56:13 -0300

    config: add and config swagger 🔧

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Healthy Brain API')
+    .setDescription('The Healthy Brain API documentation')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(8080);
 }
 bootstrap();
